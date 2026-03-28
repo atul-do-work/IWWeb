6e7933a85c35d40b4db214d92ee08c1de3d2d03f
atul-do-work <atuldowork@gmail.com>
2026-03-28 15:54:40 +0530

    chnaged to vite 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Navbar() {
   const [activeSection, setActiveSection] = useState('home');
